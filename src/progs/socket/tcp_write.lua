0ecc0fb018df99546c72326379c68f595c599127
Guilherme Salazar <gmesalazar@gmail.com>
2016-08-23 09:17:47 -0300

    add socket programs

--- src/progs/socket/tcp_write.lua
+++ src/progs/socket/tcp_write.lua
@@ -1,3 +1,3 @@
--- write stuff to TCP socket
+-- write stuff to UDP socket
 
 print(require'socket'.udp():connect("192.168.33.1", 8080):write("alo"))
--- src/progs/socket/udp_read.lua
+++ src/progs/socket/udp_read.lua
@@ -1,3 +1,3 @@
 -- read stuff from UDP socket
 
-print("read = " .. require'socket'.udp():bind("192.168.33.11", 8080):read())
+print(require'socket'.udp():bind("192.168.33.11", 8080):read())
